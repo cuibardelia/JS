1768d860ca4dc5048fdc6eaa95edc88e2be4d70f
Delia <cuibardelia@yahoo.com>
2018-03-06 21:02:37 +0200

    Tema 23 wip 2
    Almost done

--- teme/tema-23/scripts.js
+++ teme/tema-23/scripts.js
@@ -6,6 +6,8 @@ var $inputText = $('input[type="text"]');
 var $inputButton = $('input[type="button"]');
 
 
+
+
 console.log($inputText);
 console.log($inputButton);
 
@@ -20,15 +22,54 @@ $inputButton.click(function() {
         alert("Please choose a number!");
         return;
     }
+    $('.display').html('');
     displayRandomNumbers(currentValue);
 });
 
 const displayRandomNumbers = (currentValue) => {
     for (let i = 0; i < currentValue; i++) {
-        currentrandom = random(0, currentValue);
-        console.log(currentrandom);
+        currentRandom = random(0, currentValue);
+        console.log(currentRandom);
+        $('.display').append(`<p>${currentRandom}</p>`);
+        randomSize(i+1);
+        randomPosition(i+1);
+        randomColor(i+1);        
     }
 };
+
+const randomPosition = (order) => {
+    const displayWidth = $('.display').width();
+    const displayHeight = $('.display').height();    
+    let randomTop = random(0, displayHeight - 50);
+    let randomLeft = random(0, displayWidth - 50);
+    $(`p:nth-child(${order})`).css({
+        top: randomTop + 'px',
+        left: randomLeft + 'px'
+    });
+};
+
+const randomColor = (order) => {
+    let randomR = random(0, 255);
+    let randomG = random(0, 255);
+    let randomB = random(0, 255);
+    $(`p:nth-child(${order})`).css({
+        color: `rgb(${randomR}, ${randomG}, ${randomB})`
+    });
+};
+
+const randomSize = (order) => {
+    let randomFontSize = random(0, 50);
+    $(`p:nth-child(${order})`).css({
+        fontSize: randomFontSize + 'px'
+    });
+};
+
+// probleme: numbers overlay
+// zoom :-s
+// treaba cu displayHeight - 50
+
+
+
 
 
 
